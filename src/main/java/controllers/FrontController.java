0e853db569d8ac2e907347e9c7bd152c4db75c85
yukio.haseyama <mailaddress822@gmail.com>
2022-06-14 00:22:08 +0900

    FrontController作成

--- src/main/java/controllers/FrontController.java
+++ src/main/java/controllers/FrontController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,7 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="FrontController", urlPatterns={"/"})
+import actions.ActionBase;
+import actions.UnknownAction;
+import constants.ForwardConst;
+
+//フロントコントローラ
+
+@WebServlet("/")
 public class FrontController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -16,12 +23,52 @@ public class FrontController extends HttpServlet {
         super();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+             throws ServletException, IOException{
 
-    }
+                 //パラメータに該当するActionクラスのインスタンス
+                 ActionBase action = getAction(request, response);
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                 //サーブレットコンテキスト、リクエスト、レスポンスをActionインスタンスのフィールドに設定
+                 action.init(getServletContext(), request, response);
+
+                 //Actionクラスの処理を呼び出し
+                 action.process();
+             }
+
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+       throws ServletException, IOException{
         doGet(request, response);
     }
 
-}
+       //リクエストパラメータの値から該当するActionクラスのインスタンスを作成し、返却する
+       //例：パラメータがaction=Memberの場合、actions.MemberActionオブジェクト
+       @SuppressWarnings({"rawtypes","unchecked"})//コンパイラ警告を抑制
+       private ActionBase getAction(HttpServletRequest request, HttpServletResponse response) {
+           Class type = null;
+           ActionBase action = null;
+           try {
+
+               //リクエストからパラメータ"action"の値を取得（例："Member", "Song")
+               String actionString = request.getParameter(ForwardConst.ACT.getValue());
+
+               //該当するActionオブジェクトを作成（例：リクエストからパラメータ　action=Member の場合、actions.MemberActionオブジェクト）
+               type = Class.forName(String.format("actions.%sAction", actionString));
+
+               //ActionBaseのオブジェクトにキャスト（例：actions.MemberActionオブジェクト→actions.ActionBaseオブジェクト
+               action = (ActionBase) (type.asSubclass(ActionBase.class)
+                       .getDeclaredConstructor()
+                       .newInstance());
+
+           }catch(ClassNotFoundException | InstantiationException | IllegalAccessException | SecurityException
+                  |IllegalArgumentException | InvocationTargetException| NoSuchMethodException m ) {
+               //リクエストパラメータに設定されている"action"の値が不正の場合（例：action=xxxxx等、該当するActionクラスがない）
+               //エラー処理を行うActionオブジェクトを作成
+               action = new UnknownAction();
+           }
+           return action;
+           }
+       }
+
